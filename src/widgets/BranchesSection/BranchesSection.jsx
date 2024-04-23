67b56979e10b3e78fbe8cf59f10a2cf04c87a59a
bolotbekov00 <bolotbekov0606gmail.com>
2024-04-23 22:15:41 +0600

    feat: Request data for the home page

--- src/widgets/BranchesSection/BranchesSection.jsx
+++ src/widgets/BranchesSection/BranchesSection.jsx
@@ -1,16 +1,20 @@
+import { useLoaderData } from "react-router-dom";
 import style from "./BranchesSection.module.scss";
 import { BrancheCard, Typography } from "@/shared";
 
 export const BranchesSection = ({ branchesData }) => {
+  const { branches } = useLoaderData();
+  console.log(branches);
   return (
     <div className={style.branchesSection}>
       <Typography variant="h2" weight="extraBold">
         {branchesData.h2}
       </Typography>
+
       <div className={style.cardBlock}>
-        <BrancheCard cardData={branchesData} />
-        <BrancheCard cardData={branchesData} />
-        <BrancheCard cardData={branchesData} />
+        <BrancheCard {...branches} cardData={branchesData} />
+        <BrancheCard {...branches} cardData={branchesData} />
+        <BrancheCard {...branches} cardData={branchesData} />
       </div>
     </div>
   );
