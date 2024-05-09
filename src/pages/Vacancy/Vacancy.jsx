28c97d452f2d4810bc8df91a7d9b0e327d01a62c
TIMMUZY <tynchtykergesh02@gmail.com>
2024-05-09 20:50:20 +0600

    ref: Refactoring Vacancy component

--- src/pages/Vacancy/Vacancy.jsx
+++ src/pages/Vacancy/Vacancy.jsx
@@ -1,10 +1,17 @@
-import { Container, Typography, VacancyCard } from "@/shared";
+import {
+  AddFile,
+  Container,
+  CustomButton,
+  Download,
+  Typography,
+  VacancyCard,
+} from "@/shared";
 import style from "./Vacancy.module.scss";
-// import { vacancies } from "./db.json";
-import { useLoaderData } from "react-router-dom";
+import { vacancies } from "./db.json";
+// import { useLoaderData } from "react-router-dom";
 
 export const Vacancy = () => {
-  const { vacancy } = useLoaderData();
+  // const { vacancy } = useLoaderData();
 
   return (
     <>
@@ -17,16 +24,29 @@ export const Vacancy = () => {
         >
           Работа и вакансии в Бишкеке
         </Typography>
-        <Typography
-          className={style.text}
-          variant="h4"
-          weight="bold"
-          color="blue400"
-        >
-          По профессиям
-        </Typography>
+        <div className={style.inputLabel}>
+          <label className={style.addFile} htmlFor="file">
+            Загрузить файлы
+          </label>
+          <input
+            type="file"
+            id="file"
+            name="file"
+            accept=".jpg, .jpeg, .png"
+            multiple
+            className={style.none}
+          />
+          <label className={style.secondLabel} htmlFor="file">
+            {" "}
+            <Download /> Загрузить сюда файлы
+          </label>
+        </div>
+        <CustomButton className={style.btn}>
+          Отправить резюме
+          <AddFile />
+        </CustomButton>
         <div className={style.box}>
-          {vacancy?.map((vacancy) => (
+          {vacancies?.map((vacancy) => (
             <VacancyCard key={vacancy.id} {...vacancy} />
           ))}
         </div>
