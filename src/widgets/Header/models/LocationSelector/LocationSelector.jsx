f6b9125bfd28bf287b0d3a34a41acc3512fe3b32
Egor Panichkin <gogaspb98@gmail.com>
2024-05-09 11:05:10 +0600

    feat: region slice

--- src/widgets/Header/models/LocationSelector/LocationSelector.jsx
+++ src/widgets/Header/models/LocationSelector/LocationSelector.jsx
@@ -1,50 +1,49 @@
 import style from "./LocationSelector.module.scss";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import { fetchRegions, selectLocation } from "@/app/store/locationSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 export const LocationSelector = () => {
   const [city, setCity] = useState(localStorage.getItem("location"));
   const nav = useNavigate();
-  const { t } = useTranslation();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     city ? null : nav("/welcome");
   });
 
+  useEffect(() => {
+    dispatch(fetchRegions());
+  }, [dispatch]);
+
+  const regions = useSelector(selectLocation);
+
   const handleSelect = (value) => {
-    window.location.reload();
+    console.log(value);
     setCity(value);
     localStorage.setItem("location", value);
+    window.location.reload();
   };
 
   const selectActiveClass = (value) => {
-    return value === city ? style.active : style.neutral;
+    return value == city ? style.active : style.neutral;
   };
 
   return (
     <div className={style.location}>
-      <button
-        value={"Bishkek"}
-        onClick={(e) => handleSelect(e.target.value)}
-        className={selectActiveClass("Bishkek")}
-      >
-        {t("header.cities.bishkek")}
-      </button>
-      <button
-        value={"Osh"}
-        onClick={(e) => handleSelect(e.target.value)}
-        className={selectActiveClass("Osh")}
-      >
-        {t("header.cities.osh")}
-      </button>
-      <button
-        value={"Jalal-Abad"}
-        onClick={(e) => handleSelect(e.target.value)}
-        className={selectActiveClass("Jalal-Abad")}
-      >
-        {t("header.cities.jalal-abad")}
-      </button>
+      {regions.map((region, index) => {
+        return (
+          <button
+            value={region.id}
+            onClick={(e) => handleSelect(e.target.value)}
+            className={selectActiveClass(region.id)}
+            key={index}
+          >
+            {region.title}
+          </button>
+        );
+      })}
     </div>
   );
 };
